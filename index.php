03986c5ce2494263374572e3a462c1398d506c07
Luc Wybourn-Whyte <lwybourn@boudig.uk>
2020-04-22 22:05:02 +0100

    Index HTML

---
+++ index.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: LucW
+ * Date: 22/04/2020
+ * Time: 21:57
+ */
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <title>LAMP Site</title>
+</head>
+<body>
+<h1>Hello World</h1>
+</body>
+</html>
