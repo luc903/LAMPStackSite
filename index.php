3da5ad082bb095ca685202c4463699ebdad5d922
Luc Wybourn-Whyte <lwybourn@boudig.uk>
2020-04-22 22:50:37 +0100

    Correction

--- index.php
+++ index.php
@@ -11,16 +11,16 @@
 <head>
     <meta charset="utf-8">
     <title>LAMP Site</title>
-
-    <style>
-        @import url('https://fonts.googleapis.com/css2?family=Darker+Grotesque:wght@300;400;500;600;700;800;900&display=swap');
-
-        html {
-            font-family: 'Darker Grotesque', sans-serif;
-        }
-    </style>
 </head>
 <body>
+<style>
+    @import url('https://fonts.googleapis.com/css2?family=Darker+Grotesque:wght@300;400;500;600;700;800;900&display=swap');
+
+    html {
+        font-family: 'Darker Grotesque', sans-serif;
+    }
+</style>
+
 <h1>Hello World</h1>
 </body>
 </html>
